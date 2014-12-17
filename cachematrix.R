ae8e45a79f6f1bebfb1fb1ba224fde25dbb5ddf4
sampable@mac <sampable@gmail.com>
2014-12-17 23:38:06 +0800

    implement Programming Assignment 2 functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script file define functions for creating and manipulating 
+## "cachable" matrix object that can cache its inverse itself
 
-## Write a short comment describing this function
+
+## NAME : makeCacheMatrix
+## ARGUMENT(S)
+##     x : matrix object
+## RETURN TYPE : "cachable" matrix object
+## DESCRIPTION : This function turn the input matrix object
+##     into a "cachable" matrix object and return it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## NAME : cacheSolve
+## ARGUMENT(S)
+##     x : "cachable" matrix object
+##   ... : further arguments passed to solve() function if 
+##         calculation is needed
+## RETURN TYPE : matrix object
+## DESCRIPTION : This function return the inverse of the input
+##     "cachable" matrix. If cached data found, this function will 
+##     return cached data directly instead of calculate it again
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
